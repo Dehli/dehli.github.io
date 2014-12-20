4c518264e532597562ede72a939d9d774cd1ef0c
Christian Paul Dehli <cpdehli@gmail.com>
2014-12-20 09:10:24 -0500

    Reload page when submitting form.

--- js/contact_me.js
+++ js/contact_me.js
@@ -6,12 +6,21 @@ $(function() {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+            
+			
+			/*event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+
+
+			var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
 
             $.ajax({
                 url: "http://getsimpleform.com/messages?form_api_token=4f9d4fe9cd5a70758f2c79b773d8c2fa",
@@ -35,16 +44,16 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function(error) {					
+                error: function(response) {
+					for (i in response)
+						alert(response[i] + " : " + i);			
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", something went wrong. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
+                },*/
             })
         },
         filter: function() {
